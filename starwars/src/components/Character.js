3551981a9b2ef71a97171c100090522dcc7cbcb3
Zech Drinkhall <zechdrink@aol.com>
2019-02-15 10:23:51 -0800

    added hair color to each character in list

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -3,7 +3,7 @@ import React from 'react';
 // import './StarWars.css'
 
 const Character = props => {
-    return <li>{props.character.name}</li>
+    return <li>{props.character.name} - Hair-Status: {props.character.hair_color}</li>
 }
 
 export default Character;

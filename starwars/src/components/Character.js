46f04bedbca45e26413fe1aec719ac2c86f5750f
Zech Drinkhall <zechdrink@aol.com>
2019-02-15 10:42:04 -0800

    added more styling

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -3,7 +3,7 @@ import React from 'react';
 // import './StarWars.css'
 
 const Character = props => {
-    return <li>{props.character.name} - Hair-Status: {props.character.hair_color}</li>
+    return <li>{props.character.name} - <span className = "hair-style">Hair-Status: {props.character.hair_color}</span></li>
 }
 
 export default Character;
